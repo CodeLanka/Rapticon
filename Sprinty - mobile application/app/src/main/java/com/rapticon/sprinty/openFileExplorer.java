c871098585becb2753d1dbe3100ddde107c97a39
isuruamantha <isuru.amantha@gmail.com>
2016-05-12 13:55:58 +0530

    Mobile phone application(basic) is added

---
+++ Sprinty - mobile application/app/src/main/java/com/rapticon/sprinty/openFileExplorer.java
@@ -0,0 +1,34 @@
+package com.rapticon.sprinty;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+/**
+ * Created by user on 5/12/2016.
+ */
+public class openFileExplorer extends Activity {
+
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.content_main);
+        Button buttonLoadImage = (Button)findViewById(R.id.loadimage);
+
+
+        buttonLoadImage.setOnClickListener(new Button.OnClickListener(){
+
+            @Override
+            public void onClick(View arg0) {
+                // TODO Auto-generated method stub
+                Intent intent = new Intent(Intent.ACTION_PICK,
+                        android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(intent, 0);
+            }});
+    }
+}
